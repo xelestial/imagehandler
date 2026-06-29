22e77147869c3930463ab78306306088c147d4e9
Sil <30334237+xelestial@users.noreply.github.com>
2026-06-29 21:58:41 +0900

    Add batch package

---
+++ imagehandler/batch/__init__.py
@@ -0,0 +1,3 @@
+from .files import BatchResult, iter_image_files, relative_output_file, relative_output_dir
+
+__all__ = ["BatchResult", "iter_image_files", "relative_output_file", "relative_output_dir"]
