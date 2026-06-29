3e516ef33154847a8a1145b658850417d784454d
Sil <30334237+xelestial@users.noreply.github.com>
2026-06-30 00:00:03 +0900

    Export job input mover

--- imagehandler/batch/__init__.py
+++ imagehandler/batch/__init__.py
@@ -1,8 +1,8 @@
 from .files import (
     BatchResult,
     iter_image_files,
-    move_input_to_complete,
     move_input_to_failed,
+    move_input_to_job_input,
     relative_output_dir,
     relative_output_file,
 )
@@ -10,8 +10,8 @@ from .files import (
 __all__ = [
     "BatchResult",
     "iter_image_files",
-    "move_input_to_complete",
     "move_input_to_failed",
+    "move_input_to_job_input",
     "relative_output_dir",
     "relative_output_file",
 ]
